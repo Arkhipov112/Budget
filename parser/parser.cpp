bf64ccfd44532cf11330d51578c8ee9a76d3e570
Arkhipov112 <k.arkhipovv@gmail.com>
2025-05-14 21:21:45 +0700

    Update

--- parser/parser.cpp
+++ parser/parser.cpp
@@ -1,3 +1,4 @@
+#include <sstream>
 #include <stdexcept>
 
 #include "parser.hpp"
@@ -11,7 +12,7 @@ expense_tree parser::parse_expenses(std::istream& in) {
 
 	std::string line;
 	while (std::getline(in, line)) {
-		std::vector<std::string> temp = parse(split(trim(line), " "));
+		std::vector<std::string> temp = split(line, " ");
 
 		if (temp.size() != EXPENSES_PARSER_COUNT) {
 			throw (std::invalid_argument("Does not match the type"));
@@ -30,7 +31,7 @@ budget parser::parse_budget(std::istream& in) {
 
 	std::string line;
 	while (std::getline(in, line)) {
-		std::vector<std::string> temp = parse(split(trim(line), " +"));
+		std::vector<std::string> temp = split(line, " +");
 
 		std::pair<std::vector<std::string>, double> pair;
 		for (int i = 0; i < temp.size() - 1; ++i) {
@@ -46,57 +47,29 @@ budget parser::parse_budget(std::istream& in) {
 
 
 
-std::string parser::trim(const std::string& line) {
-	std::string res;
+std::vector<std::string> parser::split(const std::string& line, const std::string& delims) {
+	std::vector<std::string> res;
 
-	std::string whitespace = " \t";
+	std::istringstream iss(line);
+	std::string token;
 
-	size_t first = line.find_first_not_of(whitespace);
-	if (first == std::string::npos) {
-		return res;
-	}
-
-	size_t last = line.find_last_not_of(whitespace);
-
-	res = line.substr(first, (last - first) + 1);
-
-	return res;
-}
-
-std::string parser::split(const std::string& line, const std::string& delims) noexcept {
-	std::string res;
-
-	bool need_space = false;
-	for (char c : line) {
+	char c;
+	while (iss.get(c)) {
 		if (delims.find(c) != std::string::npos) {
-			need_space = true;
+			if (!token.empty()) {
+				res.push_back(token);
+				token.clear();
+			}
 		}
 
 		else {
-			if (need_space) {
-				res += ' ';
-				need_space = false;
-			}
-
-			res += c;
+			token += c;
 		}
 	}
 
-	return res;
-}
-
-std::vector<std::string> parser::parse(const std::string& line) {
-	std::vector<std::string> res;
-
-	size_t first = 0;
-	size_t last = line.find(' ');
-
-	while (last != std::string::npos) {
-		res.push_back(line.substr(first, last - first));
-		first = last + 1;
-		last = line.find(' ', first);
+	if (!token.empty()) {
+		res.push_back(token);
 	}
-	res.push_back(line.substr(first));
 
 	return res;
 }
--- parser/parser.hpp
+++ parser/parser.hpp
@@ -11,7 +11,5 @@ public:
 	static budget parse_budget(std::istream& in);
 
 private:
-	static std::string trim(const std::string& line);
-	static std::string split(const std::string& line, const std::string& delims) noexcept;
-	static std::vector<std::string> parse(const std::string& line);
+	static std::vector<std::string> split(const std::string& line, const std::string& delims);
 };
